421e7fb9fb9113438c6e29152fa87c7597bf5d3f
Your Name <your@email.com>
2026-05-24 08:35:02 +0530

    fix: generate valid UUIDs for customerId in load test

--- load-test.js
+++ load-test.js
@@ -86,7 +86,10 @@ function resetDatabase() {
 }
 
 function customerId() {
-  return `customer_${Math.random().toString(36).slice(2)}`
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
 
 function reservationPayload(quantity = 1) {
